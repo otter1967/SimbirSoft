559da691a226c4c8035d0610b9dd3da554b62d8f
Rado <Radoigovich@yandex.ru>
2022-02-06 02:07:16 +0300

    Initial commit

---
+++ src/main/java/com/example/simbirsoft/SimbirSoftApplication.java
@@ -0,0 +1,13 @@
+package com.example.simbirsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirSoftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimbirSoftApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simbirsoft/SimbirSoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simbirsoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirSoftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
